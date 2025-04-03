2f668ca65581ccd4d42655a0f941e016ac5223e5
gerry leo nugroho <gerryleonugroho@gmail.com>
2025-04-04 01:11:55 +0700

    fix the navbar

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -27,7 +27,7 @@ export function Navbar({
   const pathname = usePathname();
 
   return (
-    <header className="sticky top-0 z-50 w-full h-14 sm:h-16 md:h-18 lg:h-20 border-b border-gray-200/50 shadow-md bg-white/70 dark:bg-gray-900/70 dark:border-gray-800/50">
+    <header className="sticky top-0 z-50 w-full h-14 sm:h-16 md:h-18 lg:h-20 border-b border-gray-200/50 shadow-md bg-white dark:bg-gray-900 dark:border-gray-800/50">
       <div className="container mx-auto px-3 sm:px-4 md:px-6 lg:px-8 h-full flex items-center justify-between">
         <div className="flex-1">
           <Logo />
