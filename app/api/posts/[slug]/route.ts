57bc5ed700e8722c8d74a24a7f5ef3c2d1bde183
gerry leo nugroho <gerryleonugroho@gmail.com>
2025-07-09 12:56:18 +0700

    refactor(api): remove edge runtime from api routes
    The edge runtime was removed from both search and post routes as it's not needed for these endpoints

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { getContent } from '@/lib/content'
 
-export const runtime = 'edge'
+
 export async function GET(_: Request, { params }: { params: { slug: string } }) {
   const post = await getContent('posts', params.slug)
 
